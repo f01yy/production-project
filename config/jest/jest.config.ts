f0fab461c25b25cbe3a4cdb5d92de8a17c94ae55
Renat Ibraev <renibraew@yandex.ru>
2024-05-27 14:50:48 +0300

    added react testing library and tests for Button and Sidebar components

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,6 +3,8 @@
  * https://jestjs.io/docs/configuration
  */
 
+import path from 'path';
+
 export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -19,6 +21,7 @@ export default {
   coveragePathIgnorePatterns: [
     '\\\\node_modules\\\\',
   ],
+  setupFilesAfterEnv: ['<rootDir>/config/jest/setupTests.ts'],
   moduleDirectories: [
     'node_modules',
   ],
@@ -30,10 +33,17 @@ export default {
     'json',
     'node',
   ],
+  modulePaths: [
+    '<rootDir>src',
+  ],
   rootDir: '../../',
   testMatch: [
     '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
   ],
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+    '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+  },
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
 
---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -0,0 +1,16 @@
+import { fireEvent, screen } from '@testing-library/react';
+import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { Sidebar } from './Sidebar';
+
+describe('Sidebar', () => {
+  test('Test render', () => {
+    renderWithTranslation(<Sidebar />);
+    expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+  });
+
+  test('test toggle', () => {
+    const toggleBtn = screen.getByTestId('sidebar-toggle');
+    fireEvent.click(toggleBtn);
+    expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
+  });
+});
